6e0345ac74ee0e09aaa425fafd3f1aaa8c5485c7
Alexus555 <alexus555@yandex.ru>
2023-01-02 16:49:43 +0600

    New models were added

--- terminatorcrm/crm/models.py
+++ terminatorcrm/crm/models.py
@@ -184,3 +184,125 @@ class Lead(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProjectStreamStatus(models.Model):
+    name = models.CharField(max_length=255)
+
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectStream(models.Model):
+    name = models.CharField(max_length=255)
+
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    project = models.ForeignKey('Project', on_delete=models.PROTECT, null=False)
+    status = models.ForeignKey('ProjectStreamStatus', on_delete=models.PROTECT, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectReport(models.Model):
+    name = models.CharField(max_length=255)
+    category_id = models.CharField(max_length=255)
+    source_name = models.CharField(max_length=255)
+    comment = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+    image_url = models.URLField()
+
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    project_stream = models.ForeignKey('ProjectStream', on_delete=models.PROTECT, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class ImpStage(models.Model):
+    name_ru = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=255)
+
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name_ru
+
+
+class ProjectReportImpStage(models.Model):
+
+    status_id = models.BooleanField(default=False, null=False)
+
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    project_report = models.ForeignKey('ProjectReport', on_delete=models.PROTECT, null=False)
+    imp_stage = models.ForeignKey('ImpStage', on_delete=models.PROTECT, null=False)
+
+    def __str__(self):
+        return \
+            f'project_report_id: {self.project_report} - imp_stage_id: {self.imp_stage} - status_id: {self.status_id}'
+
+
+class ProjectStreamImpStage(models.Model):
+
+    rm_start_date = models.DateField(null=True)
+    rm_end_date = models.DateField(null=True)
+    fact_start_date = models.DateField(null=True)
+    fact_end_date = models.DateField(null=True)
+
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    project_stream = models.ForeignKey('ProjectStream', on_delete=models.PROTECT, null=False)
+    imp_stage = models.ForeignKey('ImpStage', on_delete=models.PROTECT, null=False)
+
+    def __str__(self):
+        return \
+            f'project_stream_id: {self.project_stream} - imp_stage_id: {self.imp_stage}'
+
+
+class PMStage(models.Model):
+    name_ru = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=255)
+
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name_ru
+
+
+class PMStep(models.Model):
+    name_ru = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=255)
+
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name_ru
+
+
+class ProjectPMStep(models.Model):
+
+    status_id = models.BooleanField(default=False, null=False)
+
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    project_pm_stage = models.ForeignKey('PMStage', on_delete=models.PROTECT, null=False)
+    pm_step = models.ForeignKey('PMStep', on_delete=models.PROTECT, null=False)
+
+    def __str__(self):
+        return \
+            f'project_pm_stage_id: {self.project_pm_stage} - pm_step_id: {self.pm_step} - status_id: {self.status_id}'
+
